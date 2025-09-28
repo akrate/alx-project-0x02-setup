970af1e1080220cdfe79607656b37cddc565d77d
akrate <oussamaakrate590@gmail.com>
2025-09-28 23:50:33 +0100

    stup projects

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-white shadow p-4">
+      <h1 className="text-xl font-bold text-blue-600">ALX Project Header</h1>
+    </header>
+  );
+};
+
+export default Header;
